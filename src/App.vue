e4e612cca0c45ff83a5cb668c89191158aebc8fc
hyeshin2530 <hyeshin2530@naver.com>
2018-09-03 14:45:04 +0900

    aside, header 추가

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/images/img_logo.png">
-    <router-view/>
+    <spm-header></spm-header>
+    <div id="wrapper">
+      <spm-aside></spm-aside>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import spmHeader from './components/header.vue'
+import spmAside from './components/aside.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    spmHeader,
+    spmAside
+  }
 }
 </script>
 
 <style>
   @import "assets/css/lib/bootstrap.css";
+  @import "css/sb-admin.css";
+  @import "css/style.css";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// const fb = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false;
 
