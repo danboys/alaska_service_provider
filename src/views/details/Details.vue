b64698cc7e50304581f9f1c9843cd106bc8c1c0b
bohyeonp <219031@naver.com>
2019-01-25 18:03:46 +0900

    서비스 목록 리스트 화면에 노출 3가지 TPYE

--- src/views/details/Details.vue
+++ src/views/details/Details.vue
@@ -10,85 +10,57 @@
     <!--//진입 경로-->
     <div class="container">
       <!--서비스분류 카테고리 1개 단위-->
-      <div v-for="(val1, key1) in depth1Data" v-if="typeof(val1) === 'object'">
-        <div class="card p-3" id="Accordion" data-children=".item" style=""  >
-          <div class="item card p-2 mb-2">
-            <div class="position-relative">
-              <a data-toggle="collapse" data-parent="#Accordion" :href="'#'+key1" aria-expanded="true" aria-controls="Accordion2">
-                {{key1}}
-              </a>
-              <!--툴팁-->
-              <a href="#" class="q_mark fa fa-exclamation-circle"></a>
-              <div class="tooltip_box">툴팁<br>툴팁입니다</div>
-              <!--//툴팁-->
-              <div class="collapse card p-2 mb-2 mt-2 position-relative" :id="key1" role="tabpanel" >
-                <div class="position-relative" v-for="(val2,key2) in val1">
+      <div v-for="(val1, key1) in depth1Data" v-if="Object.prototype.toString.call(val1) === '[object Object]'">
+      <div class="title mb-1"><strong>{{key1}}</strong></div>
+        <div class="card p-3" id="Accordion" data-children=".item">
+            <div class="item card p-2 mb-2" v-for="(val2,key2) in val1">
+              <div class="position-relative">
+                <a data-toggle="collapse" data-parent="#Accordion" :href="'#'+key2" aria-expanded="true" aria-controls="Accordion2" class="">
+                  {{key2}}</a>
+                <!--툴팁-->
+                <a href="#" class="q_mark fa fa-exclamation-circle"></a>
+                <div class="tooltip_box">툴팁입니다<br>툴팁입니다</div>
+                <!--//툴팁-->
+                <div class="card p-2 mb-2 mt-2 position-relative collapse " :id="key2" role="tabpanel" style="">
                   <p class="mb-0">{{val2}}</p>
-                  <a href="#" class="edit_list fa fa-edit"></a></div>
+                  <a href="#" class="edit_list fa fa-edit"></a>
+                </div>
               </div>
             </div>
+        </div>
+      </div>
+      <div v-else-if="Object.prototype.toString.call(val1) === '[object String]'">
+        <div class="title mb-1"><strong>{{key1}}</strong></div>
+        <div class="card p-3" id="Accordion" data-children=".item">
+          <div class="card p-2 mb-2 mt-2 position-relative collapse show" id="Accordion2" role="tabpanel" style="" >
+            <p class="mb-0">{{val1}}</p>
+            <a href="#" class="edit_list fa fa-edit"></a>
           </div>
         </div>
       </div>
-      <div v-else>
-        <div class="card p-3" id="Accordion" data-children=".item" style=""  >
-          <div class="item card p-2 mb-2">
-            <div class="position-relative">
-              <a data-toggle="collapse" data-parent="#Accordion" :href="'#'+key1" aria-expanded="true" aria-controls="Accordion2">
-                {{key1}}
-              </a>
-              <!--툴팁-->
-              <a href="#" class="q_mark  fa fa-exclamation-circle"></a>
-              <div class="tooltip_box">툴팁<br>툴팁입니다</div>
-              <!--//툴팁-->
-              <div class="collapse card p-2 mb-2 mt-2 position-relative" :id="key1" role="tabpanel"  v-if="typeof(val1) === 'string'">
-                <p class="mb-0">{{val1}}</p>
-                <a href="#" class="edit_list fa fa-edit"></a>
-              </div>
-              <div class="collapse card p-2 mb-2 mt-2 position-relative" :id="key1" role="tabpanel"  v-else>
-                <p class="mb-0">{{val2}}</p>
-                <a href="#" class="edit_list fa fa-edit"></a>
-              </div>
-            </div>
+      <div v-else-if="Object.prototype.toString.call(val1) === '[object Boolean]'">
+        <div class="title mb-1"><strong>{{key1}}</strong></div>
+        <div class="card p-3" id="Accordion" data-children=".item">
+          <div class="card p-2 mb-2 mt-2 position-relative collapse show" id="Accordion2" role="tabpanel" style="" >
+            <p class="mb-0">{{val1}}</p>
+            <a href="#" class="edit_list fa fa-edit"></a>
           </div>
         </div>
       </div>
-
-    </div><!--//container-->
-  </div><!--//wrap-->
-  <!--<div class="wrap_container">
-    <div>
-      {{this.serviceName}}
+      <div v-else >
+        <div class="title mb-1"><strong>{{key1}}</strong></div>
+        <div class="card p-3" id="Accordion" data-children=".item">
+          <div class="card p-2 mb-2 mt-2 position-relative collapse show" id="Accordion2" role="tabpanel" style="" v-for="(val2,key2) in val1">
+            <p class="mb-0">{{val2}}</p>
+            <a href="#" class="edit_list fa fa-edit"></a>
+          </div>
+        </div>
+      </div>
     </div>
-    <div>
-      <ul>
-        <li v-for="item in depth1Data">
-          <div>{{item}}</div>
-          <ul>
-            <li v-for="item in depth2Data">
-              {{item}}
-              <ul>
-                <li>
-                  <span></span>
-                  <span style="margin-left: 10px;"><a href="#" @click="showModal('ModalDetailsPopup')">수정하기</a></span>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-
-  </div>-->
-
+  </div>
 </template>
-
-
-
 <script >
-  import Vue from 'vue'
   import { mapMutations } from 'vuex'
-
   export default {
     name: 'Details',
     components: {
@@ -113,32 +85,23 @@
        * firebase 연동
        */
       fetchFirebaseData() {
-        console.log('firebase연동');
+        console.log('Firebase Data');
         firebase.database().ref(`provider/sp/${this.spName}/${this.serviceName}`).once('value')
           .then((data) => {
             /**
              * 전체 Database Object
              */
             this.oData = data.val();
-
-            //let convert = require('object-array-converter');
             this.depth1Data = data.val()
             console.log(this.depth1Data)
-
           })
           .catch((error) => {
             console.log(error)
           })
       },
-
     }
   }
-
 </script>
-
-
-
-
 <style>
   /*컨텐츠 영역*/
   .wrap {
