6a1bc52fafd3fd15424100761560e03292673051
bohyeonp <219031@naver.com>
2019-02-07 17:29:59 +0900

    상세 필드 변경 로직을 구현한다.

---
+++ src/views/modal/ModalSettingModify.vue
@@ -0,0 +1,246 @@
+<template>
+  <div>
+    <div v-if="active == 0" :data-sequence-number="sequenceNumber" class="popup_wrap m-auto">
+      <div class="popup popup_400">
+        <div class="card card_modify_SP">
+          <div class="card-header">
+            <strong>{{targetValues.key}} 하위 항목 추가/수정</strong>
+            <button class="close close_w font-xl text-right" type="button" data-dismiss="alert" aria-label="Close" @click="hideModal">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div><!--//card-header-->
+
+          <div class="card-body">
+
+            <div class="form-group mb-2">
+              <label for="company">항목 명</label>
+              <input class="form-control" id="company" type="text" placeholder="(ex.항목명)">
+            </div>
+            <div class="form-group mb-2">
+              <label for="company">설명</label>
+              <textarea class="form-control text-area-height" id="company" type="text" placeholder="(ex.항목설명)"></textarea>
+            </div>
+
+          </div><!--//card-body-->
+
+          <!--버튼-->
+          <div class="card-footer card-footer-bg-none text-right">
+            <button class="btn btn-sm btn-primary" type="submit" @click="next()">
+              <i class="fa fa-dot-circle-o"></i>수정</button>
+            <button class="btn btn-sm btn-danger" type="reset" @click="hideModal">
+              <i class="fa"></i>취소</button>
+          </div>
+          <!--//버튼-->
+
+        </div><!--//card-->
+      </div><!--//popup-->
+    </div>
+    <div v-if="active == 1" :data-sequence-number="sequenceNumber" class="popup_wrap m-auto">
+      <div class="popup">
+
+        <div class="card card_modify">
+          <div class="card-header">
+            <strong>컨텐츠 하위 항목 추가/수정</strong>
+            <button class="close close_w font-xl text-right" type="button" data-dismiss="alert" aria-label="Close" @click="hideModal">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div><!--//card-header-->
+
+          <div class="card-body">
+            <!--수정하기의 input 1칸-->
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="form-group">
+                  <p class="help-block c_light_blue">추가/수정하시겠습니까?</p>
+                </div>
+              </div>
+            </div>
+            <!--//수정하기의 input 1칸-->
+          </div><!--//card-body-->
+
+          <!--버튼-->
+          <div class="card-footer card-footer-bg-none text-right">
+            <button class="btn btn-sm btn-primary" type="submit" @click="check()">
+              <i class="fa fa-dot-circle-o"></i>확인</button>
+            <button class="btn btn-sm btn-danger" type="reset" @click="prev()">
+              <i class="fa"></i>취소</button>
+          </div>
+          <!--//버튼-->
+
+        </div><!--//card-->
+      </div><!--//popup-->
+    </div>
+    <div v-if="active == 2" :data-sequence-number="sequenceNumber" class="popup_wrap m-auto">
+      <div class="popup">
+
+        <div class="card card_modify">
+          <div class="card-header">
+            <strong>컨텐츠 하위 항목 추가/수정</strong>
+            <button class="close close_w font-xl text-right" type="button" data-dismiss="alert" aria-label="Close" @click="hideModal">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div><!--//card-header-->
+
+          <div class="card-body">
+            <!--수정하기의 input 1칸-->
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="form-group">
+                  <p class="help-block c_light_blue">추가/수정 완료되었습니다.</p>
+                </div>
+              </div>
+            </div>
+            <!--//수정하기의 input 1칸-->
+          </div><!--//card-body-->
+
+          <!--버튼-->
+          <div class="card-footer card-footer-bg-none text-right">
+            <button class="btn btn-sm btn-primary" type="reset" @click="hideModal">
+              <i class="fa"></i>닫기</button>
+          </div>
+          <!--//버튼-->
+
+        </div><!--//card-->
+      </div><!--//popup-->
+    </div>
+  </div>
+</template>
+<script>
+  import { mapMutations } from 'vuex';
+  import SubModal from '../../containers/DefaultSubModal'
+    export default {
+        name: "ModalSettingModify",
+      components: {
+        SubModal
+      },
+      data: () => {
+        return {
+          targetValues : {},
+          active : 0,
+          inputText:"",
+          inputValue:"",
+          oData: {},
+        }
+      },
+      created() {
+        this.spName = this.$route.query.spName
+        this.serviceName = this.$route.query.serviceName
+        console.log('this.targetValues :: ');
+        Object.assign(this.targetValues, this.$store.state.modalValues);
+        console.log(this.targetValues);
+      },
+      computed:{
+        sequenceNumber: function () {
+          console.log('sequenceNumber change!!');
+          return this.active
+        }
+      },
+      mounted(){
+        this.active = 0;
+      },
+      watch : {
+        componentName : () => {
+          console.log(this.componentName);
+        },
+      },
+      methods: {
+        ...mapMutations([
+          'hideModal'
+        ]),
+        next() {
+          this.inputText = $('.form-group input').val()
+          this.inputValue = $('.form-group textarea').val()
+          if(this.active !==  2){
+            this.active++;
+          }
+        },
+        prev() {
+          if(this.active !== 0){
+            this.active--;
+          }
+        },
+        check(){
+          firebase.database().ref(`provider/sp/${this.spName}/${this.serviceName}/${this.targetValues.key}`).update({
+            [this.inputText] : this.inputValue
+          }).then(() => {
+            console.log('%cSP 필드 수정 완료1','color:blue')
+            firebase.database().ref(`provider/sp/${this.spName}/${this.serviceName}/${this.targetValues.key}/${this.targetValues.valueName}`).remove(
+            ).then(() => {
+              console.log('%cSP 필드 수정 완료2','color:blue')
+              this.next();
+            }).catch((error) => {
+              console.log('%cSP 필드 수정2 중 에러가 발생하였습니다.','color:red');
+              console.log(error);
+            });
+          }).catch((error) => {
+            console.log('%cSP 필드 수정1 중 에러가 발생하였습니다.','color:red');
+            console.log(error);
+          });
+        },
+        refresh() {
+          // refresh
+          console.log('$EventBus.$emit update::');
+          this.$EventBus.$emit('update');
+          this.hideSubModal();
+        },
+      },
+    }
+</script>
+<style scoped>
+  /*팝업 공통*/
+  .popup_wrap {
+    /*사이드바 위로 보이도록 position, z-index 추가 */
+    position: absolute;
+    z-index: 9999;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0,0,0,0.5);
+    text-align: center;
+    top: 0;
+    left: 0;
+  }
+  .popup_wrap:after{
+    content:"";
+    display: inline-block;
+    height: 100%;
+    width: 0;
+    vertical-align: middle;
+  }
+  .popup {
+    width: 500px;
+    min-height: 200px;
+    display: inline-block;
+    vertical-align: middle;
+    text-align: left;
+  }
+  .popup_400 {
+    width: 400px;
+  }
+  /*팝업 닫기 버튼*/
+  .close.close_w {
+    color: #fff;
+    opacity: 1;
+  }
+  .close.close_w span {
+    box-shadow: none;
+  }
+  .close.close_w:hover{
+    color: #fff !important;
+    opacity: 1 !important;
+  }
+  /*폼 변형*/
+  .form-group {
+    margin-bottom: 0;
+  }
+  .card-footer.card-footer-bg-none {
+    background: none;
+    border-top: none;
+  }
+  .card_modify_SP .card-body .form-group {
+    position: relative;
+  }
+  /*textarea*/
+  textarea.text-area-height {
+    height: 100px;
+  }
+</style>
