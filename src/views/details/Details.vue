e893136d367e3df73e4f259752b1cecb5931e0bf
bohyeonp <219031@naver.com>
2019-02-18 14:36:14 +0900

    SPM-26 값이 없어도 수정 아이콘 노출되도록 변경

--- src/views/details/Details.vue
+++ src/views/details/Details.vue
@@ -107,7 +107,7 @@
                 <!--툴팁-->
                 <div class="card p-2 mb-2 mt-2 position-relative collapse " :id="key2" role="tabpanel" style="" >
                   <p class="mb-0">{{val2}}</p>
-                  <a v-if="val2 !== '' " href="#" class="edit_list fa fa-edit" @click="modalServiceList(key2,val2,key1,'object')"></a>
+                  <a  href="#" class="edit_list fa fa-edit" @click="modalServiceList(key2,val2,key1,'object')"></a>
                 </div>
               </div>
             </div>
@@ -118,7 +118,7 @@
           <div class="card p-3" id="Accordion" data-children=".item">
             <div class="card p-2 mb-2 mt-2 position-relative collapse show" id="Accordion2" role="tabpanel" style="" >
               <p class="mb-0">{{val1}}</p>
-              <a v-if="val1 !== '' " href="#" class="edit_list fa fa-edit" @click="modalServiceList(key1,val1,key1,'string')" ></a>
+              <a  href="#" class="edit_list fa fa-edit" @click="modalServiceList(key1,val1,key1,'string')" ></a>
             </div>
           </div>
         </div>
@@ -127,7 +127,7 @@
           <div class="card p-3" id="Accordion" data-children=".item">
             <div class="card p-2 mb-2 mt-2 position-relative collapse show" id="Accordion2" role="tabpanel" style="" >
               <p class="mb-0">{{val1}}</p>
-              <a v-if="val1 !== '' " href="#" class="edit_list fa fa-edit" @click="modalServiceList(key1,val1,key1,'boolean')"></a>
+              <a  href="#" class="edit_list fa fa-edit" @click="modalServiceList(key1,val1,key1,'boolean')"></a>
             </div>
           </div>
         </div>
@@ -136,7 +136,7 @@
           <div class="card p-3" id="Accordion" data-children=".item">
             <div class="card p-2 mb-2 mt-2 position-relative collapse show" id="Accordion2" role="tabpanel" style="" v-for="(val2,key2) in val1"  >
               <p class="mb-0">{{val2}}</p>
-              <a v-if="val2 !== '' " href="#" class="edit_list fa fa-edit" @click="modalServiceList(key1,val2,key2,'array')"></a>
+              <a  href="#" class="edit_list fa fa-edit" @click="modalServiceList(key1,val2,key2,'array')"></a>
             </div>
           </div>
         </div>
