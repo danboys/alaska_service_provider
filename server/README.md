44068f246aafb35b5b200b698a3e21b88e1ec3df
김혜신 <hyeshin2530@gmail.com>
2019-02-19 16:57:58 +0900

    # SPM-62 개선 > 내보내기 시 방어로직 추가
    - server README 파일 추가
    - checkServer 모듈 구현
    - 내보내기 실패 팝업 추가

---
+++ server/README.md
@@ -0,0 +1,16 @@
+# Downloading json file with node.js
+
+firebase에서 object 데이터를 불러온 뒤 Node Server를 이용하여 json 파일을 만들고 다운로드 한다.
+
+## Build Setup
+
+``` bash
+# move directory 
+cd server
+
+# install dependencies
+npm install
+
+# starting the Node Server
+npm start
+```
--- src/views/modal/ModalExportProvider.vue
+++ src/views/modal/ModalExportProvider.vue
@@ -69,7 +69,7 @@
 
           <!--버튼-->
           <div class="card-footer card-footer-bg-none text-right">
-            <button class="btn btn-sm btn-primary" type="submit"  @click="exportProvider">확인</button>
+            <button class="btn btn-sm btn-primary" type="submit"  @click="checkServer">확인</button>
             <button class="btn btn-sm btn-danger" type="reset" @click="hideModal">취소</button>
           </div>
           <!--//버튼-->
@@ -78,7 +78,7 @@
       </div><!--//popup-->
     </div>
     <!--내보내기 피드백 팝업-->
-    <div v-else class="popup_wrap m-auto">
+    <div v-else-if="active === 2" class="popup_wrap m-auto">
       <div class="popup">
         <div class="card card_modify">
           <div class="card-header">
@@ -95,6 +95,39 @@
                 <div class="form-group">
                   <p class="help-block">serviceProvider_{{targetProvider.spName}}.json</p>
                   <p class="help-block c_light_blue">파일이 다운로드 되었습니다.</p>
+                </div>
+              </div>
+            </div>
+            <!--//수정하기의 input 1칸-->
+          </div><!--//card-body-->
+
+          <!--버튼-->
+          <div class="card-footer card-footer-bg-none text-right">
+            <button class="btn btn-sm btn-primary" type="reset" @click="hideModal">닫기</button>
+          </div>
+          <!--//버튼-->
+
+        </div><!--//card-->
+      </div><!--//popup-->
+    </div>
+    <!--내보내기 실패 팝업-->
+    <div v-else-if="active === 3" class="popup_wrap m-auto">
+      <div class="popup">
+        <div class="card card_modify">
+          <div class="card-header">
+            <strong>내보내기 실패</strong>
+            <button class="close close_w font-xl text-right" type="button" data-dismiss="alert" aria-label="Close" @click="hideModal">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div><!--//card-header-->
+
+          <div class="card-body">
+            <!--수정하기의 input 1칸-->
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="form-group">
+                  <p class="help-block">내보내기에 실패하였습니다.</p>
+                  <p class="help-block c_light_blue">서버 연결을 확인해주세요. <a href="https://github.com/danboys/alaska_service_provider/tree/SPM-62/server" target="_blank">[확인방법]</a></p>
                 </div>
               </div>
             </div>
@@ -181,6 +214,28 @@
         if(this.active !== 0){
           this.active--;
         }
+      },
+      checkServer(){
+        var self = this;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+            if (xmlhttp.status == 200) {
+              console.log("서버 연결 확인!!");
+              this.exportProvider();
+            } else {
+              if (xmlhttp.status == 400) {
+                console.log('There was an error 400');
+              } else {
+                console.log('something else other than 200 was returned : ' + xmlhttp.status);
+              }
+              // 서버연결 안되어있음. 내보내기 실패 팝업
+              self.active = 3;
+            }
+          }
+        };
+        xmlhttp.open("GET", "http://127.0.0.1:3000/save", true);
+        xmlhttp.send();
       },
       exportProvider(){
         window.open('http://127.0.0.1:3000/save/file/'+ this.targetProvider.key);
