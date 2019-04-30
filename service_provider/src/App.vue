bcbd3fbe0403f4348c977815d79ac544225f1435
bohyeonp <219031@naver.com>
2019-04-30 16:40:27 +0900

    build

--- service_provider/src/App.vue
+++ service_provider/src/App.vue
@@ -6,26 +6,26 @@
 </template>
 
 <script>
-import AppModal from './containers/DefaultModal'
-export default {
-  name: 'App',
-  components :{
-    AppModal
+  import AppModal from './containers/DefaultModal'
+  export default {
+    name: 'App',
+    components :{
+      AppModal
+    }
   }
-}
 </script>
 
 <style lang="scss">
   // CoreUI Icons Set
-  @import '~@coreui/icons/css/coreui-icons.min.css';
+  @import '../node_modules/@coreui/icons/css/coreui-icons.min.css';
   /* Import Font Awesome Icons Set */
-  $fa-font-path: '~font-awesome/fonts/';
-  @import '~font-awesome/scss/font-awesome.scss';
+  $fa-font-path: "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/fonts" !default;
+  @import '../node_modules/font-awesome/scss/font-awesome';
   /* Import Simple Line Icons Set */
   $simple-line-font-path: '~simple-line-icons/fonts/';
   @import '~simple-line-icons/scss/simple-line-icons.scss';
   /* Import Flag Icons Set */
-  @import '~flag-icon-css/css/flag-icon.min.css';
+  @import '../node_modules/flag-icon-css/css/flag-icon.min.css';
   /* Import Bootstrap Vue Styles */
   @import 'assets/css/bootstrap-vue.css';
   // Import Main styles for this application
