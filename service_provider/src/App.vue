d25c2b074a1b60633e194c15505be1dbe8e64ceb
bohyeonp <219031@naver.com>
2019-04-30 11:28:33 +0900

    build

--- service_provider/src/App.vue
+++ service_provider/src/App.vue
@@ -16,8 +16,8 @@ export default {
 </script>
 
 <style lang="scss">
- /* // CoreUI Icons Set
-  @import '~@coreui/icons/css/coreui-icons.min.css';*/
+  // CoreUI Icons Set
+  @import '~@coreui/icons/css/coreui-icons.min.css';
   /* Import Font Awesome Icons Set */
   $fa-font-path: '~font-awesome/fonts/';
   @import '~font-awesome/scss/font-awesome.scss';
